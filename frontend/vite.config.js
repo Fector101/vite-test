132276970fbb9f8b38f6241790100894ca72e433
Fabian <fabianjoseph063@gmail.com>
2025-03-24 04:05:31 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure it's "dist" inside frontend
+    outDir: "frontend/dist", // Ensure it's "dist" inside frontend
   },
 })
