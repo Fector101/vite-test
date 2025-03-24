a3a6cb6627847753c983950d2cdb828807856a02
Fabian <fabianjoseph063@gmail.com>
2025-03-24 04:12:57 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 // Serve the built frontend
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
