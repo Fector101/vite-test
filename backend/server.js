e13fa1a3c76487b0b6d81d047ce8a6d3984564db
Fabian <fabianjoseph063@gmail.com>
2025-03-24 01:31:09 +0000

    installed a bunch of stuff

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+  };
+
+  app.use(cors(corsOptions));
+
+  app.get("/api", (req, res) => {
+    res.json({ fruits: ["apple", "strawberry", "pineapple"] });
+    });
+
+    app.listen(8080, () => {
+      console.log("Server started on port 8080");
+      });
